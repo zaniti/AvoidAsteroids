f5e7104d01cd21cbe570cd356b7ea2afead1d1ef
Zniti-Codes <57224712+Zniti-Codes@users.noreply.github.com>
2020-04-19 03:07:21 +0000

    Final changes maybe

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var astronaut
 var asteroids = []
 var astro
 let asteroid
-let bg 
+let bg
 let party
 let x = 1
 let y = 1
@@ -28,26 +28,27 @@ function preload() {
     font1 = loadFont('story/RetroGaming.ttf');
     font2 = loadFont('story/Aveny.ttf');
     font3 = loadFont('story/storyfont.ttf');
-    
+
     bg1 = loadImage('story/bg1.png')
     storyImg = loadImage('story/storyimg.png')
     music = loadSound('story/music.mp3')
-    
+
   }
 
 // Setup function
 
-function setup() { 
+function setup() {
   createCanvas(windowWidth, windowHeight);
   astronaut = new astronaut();
   music.setVolume(0.2)
   party = new Party()
-  
-  for (var i = 0; i < 5; i++) {
-    asteroids.push(new Asteroid());
-    
-  }
-} 
+
+  setTimeout(spawn, 1000);
+}
+function spawn() {
+  asteroids.push(new Asteroid());
+  setTimeout(spawn, 2000);
+}
 
 
 //Party
@@ -57,25 +58,25 @@ function Party(){
 
 //begin the party
 function enter() {
-  
+
   homePage()
   if (keyIsDown(83)) {
     party.inc=2
-   
-  
+
+
 
   }else if(keyIsDown(67)){
    party.inc=3
-   
-  
+
+
   }
 }
 
 // Draw function
 
-function draw() { 
+function draw() {
   background(bg)
-  
+
   switch(party.inc){
     case 0:
       enter()
@@ -91,42 +92,42 @@ function draw() {
           textFont(font, 30)
           text('Please "Refresh" the page to play again', width/2, height/2+100);
           return
-          
+
         }
-       
-        
+
+
         asteroids[i].render();
         asteroids[i].update();
         asteroids[i].edges();
       }
-      
-      
+
+
       astronaut.render();
       astronaut.edges();
 
       break
     }
-      case 2: 
+      case 2:
         story()
         if(keyIsDown(13)){
           party.inc=1
           music.play()
-      
+
          }
         break
-      case 3: 
+      case 3:
         credit()
         if(keyIsDown(8)){
           party.inc=0
-      
+
          }
         break
-      
-      
+
+
 
   }
-  
-  
+
+
 }
 
 // Astronaut shape function
@@ -136,7 +137,7 @@ function astronaut() {
     this.r = 65;
 
     this.hits = function(asteroid) {
-        
+
       var d = dist(x, y, asteroid.pos.x, asteroid.pos.y);
       if (d < this.r + asteroid.r/2) {
         return true;
@@ -145,7 +146,7 @@ function astronaut() {
       }
     }
 
-    
+
     this.render = function() {
       push();
       let targetX = mouseX
@@ -156,7 +157,7 @@ function astronaut() {
         let dy = targetY - y
         y += dy * easing
         astro.resize(150,0)
-    
+
       image(astro, x-70, y-70)
       translate(x , y)
       noFill()
@@ -164,7 +165,7 @@ function astronaut() {
       triangle(-this.r, this.r, this.r, this.r, 20, -this.r)
       pop()
     }
-    
+
     this.edges = function() {
       if (this.pos.x > width + this.r) {
         this.pos.x = -this.r;
@@ -178,23 +179,23 @@ function astronaut() {
       }
     }
 
-    
+
   }
-  
+
   // Asteraoid shape function
 
   function Asteroid() {
-         this.pos = createVector(random(width), random(height));
+         this.pos = createVector(-(random(width)), -(random(height)));
          this.r = 100;
          this.vel = p5.Vector.random2D();
 
-    
+
     this.update = function() {
       this.pos.add(this.vel);
     }
 
     asteroid.resize(this.r,0);
-    
+
     this.render = function(){
         image(asteroid, this.pos.x-(this.r/2), this.pos.y-(this.r/2));
         push();
@@ -202,13 +203,13 @@ function astronaut() {
         noFill();
         translate(this.pos.x, this.pos.y);
         ellipse(0, 0, this.r);
-    
-    
-    
+
+
+
         pop();
       }
-  
-    
+
+
     this.edges = function() {
       if (this.pos.x > width + this.r) {
         this.pos.x = -this.r;
@@ -221,7 +222,7 @@ function astronaut() {
         this.pos.y = height + this.r;
       }
     }
-    
+
   }
 
   // Home page function
@@ -244,10 +245,10 @@ function astronaut() {
     text('Press "C" for credits' , width/2, height/2+100)
 
 
-    
+
 
   }
-  
+
   function credit(){
 
     background(bg)
@@ -286,22 +287,20 @@ function astronaut() {
   function story(){
     background(bg1)
     storyImg.resize(300,0)
-    image(storyImg,width/2-150,100)
+    image(storyImg,width/2-150,50)
 
     textFont(font3, 35)
     textAlign(CENTER)
     fill(255)
-    text('“3, 2, 1...” were the last words i remember before the crash of our ship ' , width/2, 450)
-    text('am i the only survivor of the crew ? ' , width/2, 500)
-    text('The only way for me to survive is to reach the emergency recovery kit' , width/2, 550)
-    text('but first i need you to help me avoid those asteroids' , width/2, 600)
-    text('I have two kids, and i will never forgive you if you let me die' , width/2, 650)
+    text('“3, 2, 1...” were the last words i remember before the crash of our ship ' , width/2, 400)
+    text('am i the only survivor of the crew ? ' , width/2, 450)
+    text('The only way for me to survive is to reach the emergency recovery kit' , width/2, 500)
+    text('but first i need you to help me avoid those asteroids' , width/2, 550)
+    text('I have two kids, and i will never forgive you if you let me die' , width/2, 600)
     textFont(font3, 30)
-    text('Press "Enter" to continue ..' , width/2+500, 750)
-   
-    
+    text('Press "Enter" to continue ..' , width/2+500, 700)
+
+
 
 
   }
-
- 
